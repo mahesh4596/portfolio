fe9b64ffb5f488af1c50fd699c1559da1116a03f
Mahesh Singla <maheshsingla2007@gmail.com>
2026-09-15 15:07:53 +0530

    fix: load project screenshots from public assets

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,5 +1,5 @@
-import medihopeImg from '../assets/medihope-cover.jpg';
-import ssgsImg from '../assets/ssgs-cover.jpg';
+const medihopeImg = '/assets/medihope-cover.jpg';
+const ssgsImg = '/assets/ssgs-cover.jpg';
 
 export const projects = [
   {
